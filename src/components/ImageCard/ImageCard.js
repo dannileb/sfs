f5ac5646f3f94ebe3767fdc1f0f7b1b27be30d78
Danila Lebedev <l1endqwsa@gmail.com>
2023-08-29 13:03:34 +0300

    fixed image card text layout

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -2,35 +2,38 @@ import './ImageCard.css'
 import {useState} from "react";
 
 function ImageCard({imageSrc, imageCapture, imageText}){
+    //Text display hook
     const [flipped, setFlipped] = useState(false);
 
     return (
-        flipped
+            flipped
             ?
-        <div className="ImageCard ImageCard_flipped" onClick={()=>setFlipped(!flipped)}>
-            <p className="ImageCard_Text ImageCard_Text_flipped">
-                {
-                    imageText.map(text=>{
-                        return (
-                            <>
-                                <span className="ImageCard_Paragraph">{text}</span>
-                                <br/>
-                            </>
+            /*Text displays*/
+            <div className="ImageCard ImageCard_flipped" onClick={()=>setFlipped(!flipped)}>
+                <p className="ImageCard_Text ImageCard_Text_flipped">
+                        {
+                            imageText.map(text=>{
+                                return (
+                                    <>
+                                        <span className="ImageCard_Paragraph">{text}</span>
+                                    </>
+                                )
+                            })
+                        }
 
-                    )
-                    })
-                }
+                    </p>
+                <img src={imageSrc} className="ImageCard-Image ImageCard-Image_flipped" alt="highSchool"/>
 
-            </p>
-            <img src={imageSrc} className="ImageCard-Image ImageCard-Image_flipped" alt="highSchool"/>
-            <p className="ImageCard-Capture ImageCard-Capture_flipped">{imageCapture}</p>
-        </div>
+                <p className="ImageCard-Capture ImageCard-Capture_flipped">{imageCapture}</p>
+            </div>
             :
-        <div className="ImageCard" onClick={()=>setFlipped(!flipped)}>
-            <p className="ImageCard_Text">{imageText[0]}</p>
-            <img src={imageSrc} className="ImageCard-Image" alt="highSchool"/>
-            <p className="ImageCard-Capture">{imageCapture}</p>
-        </div>
+            /*Image displays*/
+            <div className="ImageCard" onClick={()=>setFlipped(!flipped)}>
+                <p className="ImageCard_Text">{imageText[0]}</p>
+                <img src={imageSrc} className="ImageCard-Image" alt="highSchool"/>
+
+                <p className="ImageCard-Capture">{imageCapture}</p>
+            </div>
     )
 }
 export default ImageCard;
---
+++ src/pages/Story/Story.js
@@ -0,0 +1,7 @@
+import './Story.css';
+
+function Story(){
+
+}
+
+export default Story
